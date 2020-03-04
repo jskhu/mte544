dda397de688752013ba72f9bfe6d4bfe8178d8f9
Jordan Hu <jhu.jordan@gmail.com>
2020-03-04 16:28:44 -0500

    Fixed OccGrid class

--- pkgs/lab2_localization/src/OccGrid.cpp
+++ pkgs/lab2_localization/src/OccGrid.cpp
@@ -7,26 +7,27 @@
 
 #include <map>
 
-OccGrid::OccGrid(float width, float height, float resolution,
+OccGrid::OccGrid(int cells_x, int cells_y, float resolution,
                  const Eigen::Vector3f& origin,
-                 const std::map<UpdateState, float>& update_values) :
-    m_width(width), m_height(height),
+                 std::map<UpdateState, float>& update_values) :
+    m_cells_x(cells_x), m_cells_y(cells_y),
     m_resolution(resolution), m_origin(origin),
     is_init(false), m_update_values(update_values) {
+        m_width = resolution * m_cells_x;
+        m_height = resolution * m_cells_y;
 }
 
 void OccGrid::init_publisher(ros::NodeHandle& n, const std::string& topic) {
     // Populate the m_grid message
-    m_msg.info.height = m_height;
-    m_msg.info.width = m_width;
+    m_msg.info.height = m_cells_y;
+    m_msg.info.width = m_cells_x;
     m_msg.info.resolution = m_resolution;
     m_msg.info.origin.position.x = m_origin[0];
     m_msg.info.origin.position.y = m_origin[1];
     m_msg.info.origin.position.z = m_origin[2];
 
-    m_msg.data.resize(m_height * m_width);
-    for (auto& pixel : m_msg.data) {
-        pixel = m_update_values[UNKNOWN];
+    for (int i = 0; i < m_cells_x * m_cells_y; ++i) {
+        m_msg.data.push_back(100 * m_update_values[UNKNOWN]);
     }
 
    m_occ_publisher = n.advertise<nav_msgs::OccupancyGrid>(topic, 1);
@@ -37,18 +38,18 @@ void OccGrid::init_publisher(ros::NodeHandle& n, const std::string& topic) {
 OccGrid::~OccGrid() {}
 
 int8_t OccGrid::operator[] (const Eigen::Vector2i& coord) const {
-    return m_msg.data[coord[1] * m_width + coord[0]];
+    return m_msg.data[coord[1] * m_cells_x + coord[0]];
 }
 
 int8_t& OccGrid::operator[] (const Eigen::Vector2i& coord) {
-    return m_msg.data[coord[1] * m_width + coord[0]];
+    return m_msg.data[coord[1] * m_cells_x + coord[0]];
 }
 
 void OccGrid::update_pixel(const Eigen::Vector2i& coord, UpdateState state) {
-    float l = log_odds((*this)[coord]) +
+    float l = log_odds((*this)[coord] * 0.01f) +
         log_odds(m_update_values[state]) -
         log_odds(m_update_values[UNKNOWN]);
-    (*this)[coord] = prob(l);
+    (*this)[coord] = prob(l) * 100;
 }
 
 void OccGrid::publish() {
@@ -58,6 +59,22 @@ void OccGrid::publish() {
     }
     m_msg.info.map_load_time = ros::Time::now();
     m_occ_publisher.publish(m_msg);
+}
+
+bool OccGrid::convert_to_occ_coords(double x_world, double y_world, Eigen::Vector2i& point_occ) {
+    double x_occ = x_world - m_origin[0];
+    double y_occ = y_world - m_origin[1];
+    ROS_INFO_STREAM("x_occ: " << x_occ << " y_occ: " << y_occ);
+    if (x_occ < 0 || x_occ > m_width) {
+        return false;
+    }
+    if (y_occ < 0 || y_occ >= m_height) {
+        return false;
+    }
+
+    point_occ[0] = (x_occ + 1e-5) / m_width * m_cells_x;
+    point_occ[1] = (y_occ + 1e-5) / m_height * m_cells_y;
+    return true;
 }
 
 float OccGrid::log_odds(float prob) {
--- pkgs/lab2_localization/src/occ_example.cpp
+++ pkgs/lab2_localization/src/occ_example.cpp
@@ -23,41 +23,62 @@
 
 ros::Publisher pose_publisher;
 tf::Transform world_t_robot;
+std::map<UpdateState, float> update_states;
+OccGrid occ_grid(2000, 2000, 0.01, Eigen::Vector3f(-10, -10, 0), update_states);
 
 // function for line generation from
-// https://www.geeksforgeeks.org/bresenhams-line-generation-algorithm/
+// https://www.thecrazyprogrammer.com/2017/01/bresenhams-line-drawing-algorithm-c-c.html
 std::vector<Eigen::Vector2i> get_bresenham_points(int x1, int y1, int x2,
                                                   int y2) {
 
   std::vector<Eigen::Vector2i> intermediate_points;
 
-  int m_new = 2 * (y2 - y1);
-  int slope_error_new = m_new - (x2 - x1);
-  for (int x = x1, y = y1; x <= x2; x++) {
-    intermediate_points.push_back(Eigen::Vector2i(x, y));
-
-    // Add slope to increment angle formed
-    slope_error_new += m_new;
-
-    // Slope error reached limit, time to
-    // increment y and update slope error.
-    if (slope_error_new >= 0) {
-      y++;
-      slope_error_new -= 2 * (x2 - x1);
-    }
+  if (x1 > x2) {
+    int t = x1;
+    x1 = x2;
+    x2 = t;
+    t = y1;
+    y1 = y2;
+    y2 = t;
   }
+
+  int dx = x2 - x1;
+  int dy = y2 - y1;
+
+  int x = x1;
+  int y = y1;
+
+  int p = 2 * dy - dx;
+
+  while (x < x2) {
+    if (p >= 0) {
+      intermediate_points.push_back(Eigen::Vector2i(x, y));
+      y = y + 1;
+      p = p + 2 * dy - 2 * dx;
+    } else {
+      intermediate_points.push_back(Eigen::Vector2i(x, y));
+      p = p + 2 * dy;
+    }
+    x = x + 1;
+    // ROS_INFO_STREAM("x: " << x << " x1: " << x1 << " x2: " << x2);
+  }
+
   return intermediate_points;
 }
 
 void pose_callback(
     const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
   tf::poseMsgToTF(msg->pose.pose, world_t_robot);
+  world_t_robot.getOrigin().setZ(0);
 }
 
 void scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg) {
   tf::Vector3 cur_origin = world_t_robot.getOrigin();
 
   for (std::size_t i = 0; i < msg->ranges.size(); ++i) {
+    if (std::isnan(msg->ranges[i])) {
+      continue;
+    }
     float cur_angle = msg->angle_min + msg->angle_increment * i;
 
     float x = msg->ranges[i] * cos(cur_angle);
@@ -69,9 +90,25 @@ void scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg) {
 
     tf::Transform world_t_laser = world_t_robot * robot_t_laser;
 
-    get_bresenham_points(cur_origin.getX(), cur_origin.getY(),
-                         world_t_laser.getOrigin().getX(),
-                         world_t_laser.getOrigin().getY());
+    // TODO: Add bresenham, hack for now
+    double cur_origin_x = cur_origin.getX();
+    double cur_origin_y = cur_origin.getY();
+    double laser_x = world_t_laser.getOrigin().getX();
+    double laser_y = world_t_laser.getOrigin().getY();
+
+    std::vector<std::pair<double,double>> points = {{cur_origin_x, cur_origin_y},
+                                     {laser_x, laser_y}};
+
+    for (int i = 0; i < points.size() - 1; i++) {
+      Eigen::Vector2i point;
+      if (occ_grid.convert_to_occ_coords(points[i].first, points[i].second, point)) {
+        occ_grid.update_pixel(point, FREE);
+      }
+    }
+    Eigen::Vector2i point;
+    if (occ_grid.convert_to_occ_coords(points[points.size()-1].first, points[points.size()-1].second, point)) {
+      occ_grid.update_pixel(point, OCCUPIED);
+    }
   }
 }
 
@@ -80,9 +117,6 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "occ");
   ros::NodeHandle n;
 
-  bool use_sim;
-  n.param("use_sim", use_sim, true);
-
   pose_publisher = n.advertise<geometry_msgs::PoseStamped>("/pose", 1, true);
 
   ros::Subscriber scan_subscriber = n.subscribe("/scan", 1, scan_callback);
@@ -90,22 +124,13 @@ int main(int argc, char **argv) {
   ros::Subscriber pose_subscriber =
       n.subscribe("/indoor_pos", 1, pose_callback);
 
-   std::map<UpdateState, float> update_states;
-   update_states[UNKNOWN] = 0.5;
-   update_states[FREE] = 0.3;
-   update_states[OCCUPIED] = 0.7;
-
-  OccGrid occ_grid(3.0f, 3.0f, 1.0f, Eigen::Vector3f(0, 0, 0), update_states);
+  // Initialize the occupancy grid
+  // For some reason, can't add members to update states outside function
+  // Add here instead
+  update_states[UNKNOWN] = 0.5;
+  update_states[FREE] = 0.3;
+  update_states[OCCUPIED] = 0.6;
   occ_grid.init_publisher(n, "/occ");
-
-  // occ_grid.data.resize(9);
-  int foo[] = {100, 0, 0, 0, 0, 100, 100, 100, 0};
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      ROS_INFO_STREAM(foo[i * 3 + j]);
-      occ_grid[Eigen::Vector2i(j, i)] = foo[i * 3 + j];
-    }
-  }
 
   // Set the loop rate`
   ros::Rate loop_rate(20); // 20Hz update rate
@@ -113,8 +138,6 @@ int main(int argc, char **argv) {
   while (ros::ok()) {
     loop_rate.sleep(); // Maintain the loop rate
     ros::spinOnce();   // Check for new messages
-
-    // Draw Curves
     occ_grid.publish();
   }
 
