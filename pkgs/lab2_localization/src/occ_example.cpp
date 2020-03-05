103949075b71d7bd3212b7e697261bc4477970f3
Kevin Lam <kvnlam23@gmail.com>
2020-03-04 21:02:26 -0500

    Fixed bresenham

--- pkgs/lab2_localization/src/occ_example.cpp
+++ pkgs/lab2_localization/src/occ_example.cpp
@@ -26,41 +26,51 @@ tf::Transform world_t_robot;
 std::map<UpdateState, float> update_states;
 OccGrid occ_grid(2000, 2000, 0.01, Eigen::Vector3f(-10, -10, 0), update_states);
 
-// function for line generation from
-// https://www.thecrazyprogrammer.com/2017/01/bresenhams-line-drawing-algorithm-c-c.html
+// Function for line generation adapted from
+// http://tech-algorithm.com/articles/drawing-line-using-bresenham-algorithm/
 std::vector<Eigen::Vector2i> get_bresenham_points(int x1, int y1, int x2,
                                                   int y2) {
 
   std::vector<Eigen::Vector2i> intermediate_points;
 
-  if (x1 > x2) {
-    int t = x1;
-    x1 = x2;
-    x2 = t;
-    t = y1;
-    y1 = y2;
-    y2 = t;
+  int w = x2 - x1;
+  int h = y2 - y1;
+  int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+  if (w < 0)
+    dx1 = -1;
+  else if (w > 0)
+    dx1 = 1;
+  if (h < 0)
+    dy1 = -1;
+  else if (h > 0)
+    dy1 = 1;
+  if (w < 0)
+    dx2 = -1;
+  else if (w > 0)
+    dx2 = 1;
+  int longest = abs(w);
+  int shortest = abs(h);
+  if (!(longest > shortest)) {
+    longest = abs(h);
+    shortest = abs(w);
+    if (h < 0)
+      dy2 = -1;
+    else if (h > 0)
+      dy2 = 1;
+    dx2 = 0;
   }
-
-  int dx = x2 - x1;
-  int dy = y2 - y1;
-
-  int x = x1;
-  int y = y1;
-
-  int p = 2 * dy - dx;
-
-  while (x < x2) {
-    if (p >= 0) {
-      intermediate_points.push_back(Eigen::Vector2i(x, y));
-      y = y + 1;
-      p = p + 2 * dy - 2 * dx;
+  int numerator = longest >> 1;
+  for (int i = 0; i <= longest; i++) {
+    intermediate_points.push_back(Eigen::Vector2i(x1, y1));
+    numerator += shortest;
+    if (!(numerator < longest)) {
+      numerator -= longest;
+      x1 += dx1;
+      y1 += dy1;
     } else {
-      intermediate_points.push_back(Eigen::Vector2i(x, y));
-      p = p + 2 * dy;
+      x1 += dx2;
+      y1 += dy2;
     }
-    x = x + 1;
-    // ROS_INFO_STREAM("x: " << x << " x1: " << x1 << " x2: " << x2);
   }
 
   return intermediate_points;
@@ -96,17 +106,20 @@ void scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg) {
     double laser_x = world_t_laser.getOrigin().getX();
     double laser_y = world_t_laser.getOrigin().getY();
 
-    std::vector<std::pair<double,double>> points = {{cur_origin_x, cur_origin_y},
-                                     {laser_x, laser_y}};
+    std::vector<Eigen::Vector2i> points = get_bresenham_points(cur_origin_x,
+    cur_origin_y, laser_x, laser_y);
 
     for (int i = 0; i < points.size() - 1; i++) {
       Eigen::Vector2i point;
-      if (occ_grid.convert_to_occ_coords(points[i].first, points[i].second, point)) {
+      if (occ_grid.convert_to_occ_coords(points[i].x(), points[i].y(),
+                                         point)) {
         occ_grid.update_pixel(point, FREE);
       }
     }
     Eigen::Vector2i point;
-    if (occ_grid.convert_to_occ_coords(points[points.size()-1].first, points[points.size()-1].second, point)) {
+    if (occ_grid.convert_to_occ_coords(points[points.size() - 1].x(),
+                                       points[points.size() - 1].y(),
+                                       point)) {
       occ_grid.update_pixel(point, OCCUPIED);
     }
   }
